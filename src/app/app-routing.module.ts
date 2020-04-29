12b865cc48f0e55dd2e980ebc7d5aa795208295d
Amruta Naidu <amruta.naidu26@gmail.com>
2020-04-28 20:15:45 -0400

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes} from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
 
-
-const routes: Routes = [];
+const appRoutes: Routes = [
+    {path: '', redirectTo: '/home', pathMatch: 'full'}
+    ,{path: 'home', component: HomeComponent}
+    ,{path: 'about', component: AboutComponent }
+    ,{path: 'admin', component: AdminComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [ RouterModule.forRoot(appRoutes) ],
+    exports: [ RouterModule],
+    providers: [ ]
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule {}
+
